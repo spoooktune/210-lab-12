65b2c1b9594016bc97a8ca486e958aad7aba2953
spoooktune <asriprasana5@gmail.com>
2025-09-21 18:56:28 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+// Have array of student names for a class roster
+int main() {
+    const int SIZE = 30;
+    array <string, SIZE> roster;
+    for (int i = 0; i < SIZE; i++){
+        // input names from file here
+    }
+    return 0;
+}
